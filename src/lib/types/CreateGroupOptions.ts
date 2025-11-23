9c31769ae1fcca78158df1c331c70f2c00b01b4f
Nicole Gomez Saaavedra <132105483+nicolegs22@users.noreply.github.com>
2025-11-23 02:32:45 -0800

    Delete src directory

--- src/lib/types/CreateGroupOptions.ts
+++
@@ -1,9 +0,0 @@
-import { DisappearingMessageSettings } from '../DisappearingMessageSettings'
-
-export type CreateGroupOptions = {
-  permissionLevel?: 'all_members' | 'admin_only' | undefined
-  name?: string | undefined
-  imageUrl?: string | undefined
-  description?: string | undefined
-  disappearingMessageSettings?: DisappearingMessageSettings | undefined
-}
--- src/lib/types/SendOptions.ts
+++
@@ -1,7 +0,0 @@
-import { ContentTypeId } from './ContentCodec'
-
-export type SendOptions = {
-  contentType?: ContentTypeId
-  /** Whether to send a push notification. Defaults to the codec's shouldPush behavior if not specified. */
-  shouldPush?: boolean
-}
